6bba5122e2e991d18aa96ba91b9af4fd1406e279
Ivan5646 <Mike000027@yandex.ru>
2020-07-04 22:31:38 +0700

    auth btn started

---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+function Header() {
+    return (
+        <AppBar>
+            <div>Home</div>  
+            <div>Login</div> 
+        </AppBar>
+    )
+}
+
+export default Header;
